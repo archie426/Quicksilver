71a1e40087038c40a24e48d0ddd4ce93c5b232a1
hiilikewiki56 <archiestride42@gmail.com>
2021-03-03 10:55:02 +0000

    push rest of java changes

--- JavaLoader/dllmain.cpp
+++ JavaLoader/dllmain.cpp
@@ -7,10 +7,17 @@
 
 #include "jni.h"
 
-const auto MainClass = "Load";
-const auto MainMethod = "MainMethod";
-const auto LibraryAddress = "C:\\aaaa.jar";
-const auto MainMethodSignature = "MainMethodSignature";
+#define JNI_VERSION_1_1 0x00010001
+#define JNI_VERSION_1_2 0x00010002
+#define JNI_VERSION_1_4 0x00010004
+#define JNI_VERSION_1_6 0x00010006
+#define JNI_VERSION_1_8 0x00010008
+#define JNI_VERSION_9   0x00090000
+
+const std::string MainClass = "Load";
+const std::string MainMethod = "MainMethod";
+const std::string LibraryAddress = "C:\\aaaa.jar";
+const std::string MainMethodSignature = "MainMethodSignature";
 
 
 void DebugLog(std::string datas) {
@@ -19,6 +26,32 @@ void DebugLog(std::string datas) {
         printf(datas.c_str());
         printf("\n");
    
+}
+
+BOOL GetJVM(JavaVM* &vm, JNIEnv* &jniEnvironment)
+{
+    jsize vmsAmt;
+	//giving it big and large
+    JNI_GetCreatedJavaVMs(nullptr, NULL, &vmsAmt); //obtain buffer length so this method is now safe 1!!1!!111
+	if (vmsAmt <= 0)
+	{
+        JavaVMInitArgs vm_args;
+        vm_args.version = JNI_VERSION_1_2;
+        vm_args.nOptions = 0;
+        vm_args.ignoreUnrecognized = false;
+        // ReSharper disable once CppCStyleCast
+        vm_args.options[0].optionString = (char*)MainClass.c_str();
+        vm_args.ignoreUnrecognized = false;
+        DebugLog("Creating JVM");
+        jint res = JNI_CreateJavaVM(&vm, reinterpret_cast<void**>(&jniEnvironment), &vm_args);
+        return FALSE;
+	}
+    auto* const buff = new JavaVM*[vmsAmt];
+    JNI_GetCreatedJavaVMs(buff, vmsAmt, &vmsAmt);
+    DebugLog("JVM already exists!");
+    vm = buff[0];
+    vm->GetEnv(reinterpret_cast<void**>(&jniEnvironment), JNI_VERSION_9);
+    return TRUE;
 }
 
 BOOL APIENTRY DllMain( HMODULE hModule,
@@ -35,19 +68,23 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     JavaVM* vm;
     JNIEnv* environment;
     JavaVMInitArgs vm_args;
-    vm_args.version = JNI_VERSION_1_2;
-    vm_args.nOptions = 0;
-    vm_args.ignoreUnrecognized = false;
-	vm_args.options[0].optionString = const_cast<char*>(LibraryAddress);
-    vm_args.ignoreUnrecognized = false;
-    DebugLog("Creating JVM");
-    jint res = JNI_CreateJavaVM(&vm, reinterpret_cast<void**>(&environment), &vm_args);
+    DebugLog("Getting Java VM");
+    if (!GetJVM(vm, environment))
+    {
+        DebugLog("Could not find a JVM so created one");
+    }
+    else
+        DebugLog("Found a JVM");
+	
     DebugLog("Finding class....");
-    const jclass mainClass = environment->FindClass(MainClass);
+    auto* const mainClass = environment->FindClass(MainClass.c_str());
     DebugLog("Finding method.......");
-    const jmethodID mainMethod = environment->GetStaticMethodID(mainClass, MainMethod, MainMethodSignature);
+    auto* const main_method = environment->GetStaticMethodID(mainClass, MainMethod.c_str(), MainMethodSignature.c_str());
+    DebugLog("Attaching to thread...");
+    vm->AttachCurrentThread(reinterpret_cast<void**>(&environment), NULL);
     DebugLog("Invoking method...");
-    environment->CallStaticVoidMethod(mainClass, mainMethod);
+    environment->CallStaticVoidMethod(mainClass, main_method);
+    vm->DetachCurrentThread();
 
 }
 
